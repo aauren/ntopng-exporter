8d97c7b45fd92ab8e01eccd0d8b65768d5363aae
Aaron U'Ren <aauren@gmail.com>
2026-06-27 13:43:31 -0500

    chore: upgrade golangci-lint 2.11 -> 2.12

--- internal/ntopng/utils.go
+++ internal/ntopng/utils.go
@@ -9,7 +9,7 @@ import (
 
 func getHttpResponseBody(client *http.Client, req *http.Request) (*[]byte, int, error) {
 	var body []byte
-	resp, err := client.Do(req) //nolint:gosec // URL is constructed from trusted application configuration, not user input
+	resp, err := client.Do(req)
 	if err != nil {
 		return &body, 0, err
 	}
